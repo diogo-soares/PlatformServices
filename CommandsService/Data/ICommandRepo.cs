dd3ae123654558b6474b9d71abc1aae6eec58f20
diogo-soares <diogosoaresk8@hotmail.com>
2022-03-20 22:42:27 -0300

    create repository and interface

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        // Platforms
+        IEnumerable<Platform> GetallPlatform();
+        void CreatePlatform(Platform plat);
+        bool PlatformExists(int platformId);
+
+        // Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
