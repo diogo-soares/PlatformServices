1f9158f5d91ac22f1794f4b5e42c794d9e92ec1d
diogo-soares <diogosoaresk8@hotmail.com>
2022-03-20 22:13:48 -0300

    create appdbcontext

--- CommandsService/Models/Platform.cs
+++ CommandsService/Models/Platform.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,12 @@ namespace CommandsService.Models
 {
     public class Platform
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+        [Required]
         public int ExternalID { get; set; }
+        [Required]
         public string Name { get; set; }
 
         public ICollection<Command> Commands { get; set; } = new List<Command>();
